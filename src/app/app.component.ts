7ff3a39215052800c8a295537c8ff99e8862b4d9
blint <korpaibalint@gmail.com>
2024-05-14 00:53:39 +0200

    restructuring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,95 +1,159 @@
-import { CurrencyPipe } from '@angular/common';
-import { Component, ElementRef } from '@angular/core';
+import { CurrencyPipe, NgClass, NgIf } from '@angular/common';
+import { Component, ElementRef, OnInit } from '@angular/core';
 import {
   FormControl,
   ReactiveFormsModule,
   Validators,
   FormGroup,
+  FormBuilder,
 } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CurrencyPipe, ReactiveFormsModule],
+  imports: [RouterOutlet, CurrencyPipe, ReactiveFormsModule, NgClass, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  tipAmountPerPerson: number = 0;
-  totalPerPerson: number = 0;
-  PreviousTipPercentage = 0;
+export class AppComponent implements OnInit {
+  calculatorForm: FormGroup;
 
-  calculatorForm = new FormGroup({
-    billAmount: new FormControl<number | null>(null, [Validators.required]),
-    tipPercentage: new FormControl<number | null>(null, [Validators.required]),
-    numOfPeople: new FormControl<number | null>(null, [
-      Validators.required,
-      Validators.min(1),
-    ]),
-  });
+  tipAmountPerPerson = 0;
+  totalPerPerson = 0;
 
-  constructor(private elementRef: ElementRef) {}
+  ngOnInit(): void {
+    this.calculatorForm.valueChanges.subscribe(() => {
+      this.calculateOutput();
+    });
+
+    this.calculatorForm
+      .get('customTipPercentage')
+      ?.valueChanges.subscribe(() => {
+        this.uncheckTipRadios();
+      });
+
+    this.calculatorForm.get('tipPercentage')?.valueChanges.subscribe(() => {
+      this.emptyCustomTipPercentage();
+    });
+  }
+
+  constructor(private fb: FormBuilder, private elementRef: ElementRef) {
+    this.calculatorForm = this.fb.group({
+      billAmount: new FormControl('', Validators.min(0)),
+      tipPercentage: '',
+      customTipPercentage: new FormControl('', Validators.min(0)),
+      numOfPeople: new FormControl('', [
+        Validators.required,
+        Validators.min(1),
+        Validators.pattern('^[0-9]*$'),
+      ]),
+    });
+  }
 
   isDefault() {
-    const billAmount = this.calculatorForm.get('billAmount')?.value || 0;
-    const tipPercentage = this.calculatorForm.get('tipPercentage')?.value || 0;
-    const numOfPeople = this.calculatorForm.get('numOfPeople')?.value || 0;
+    const billAmount = this.calculatorForm.get('billAmount')?.value;
+    const tipPercentage = this.calculatorForm.get('tipPercentage')?.value;
+    const customTipPercentage = this.calculatorForm.get(
+      'customTipPercentage'
+    )?.value;
+    const numOfPeople = this.calculatorForm.get('numOfPeople')?.value;
 
-    return billAmount == 0 && tipPercentage == 0 && numOfPeople == 0;
+    return (
+      billAmount == '' &&
+      tipPercentage == '' &&
+      numOfPeople == '' &&
+      customTipPercentage == ''
+    );
   }
 
   resetForm() {
     this.calculatorForm.reset({
-      billAmount: 0,
-      tipPercentage: 0,
-      numOfPeople: 0,
+      billAmount: '',
+      tipPercentage: '',
+      customTipPercentage: '',
+      numOfPeople: '',
     });
+    this.tipAmountPerPerson = 0;
+    this.totalPerPerson = 0;
   }
 
-  calculateOutput(e: Event) {
-    const billAmount = this.calculatorForm.get('billAmount')?.value || 0;
-    const tipPercentage = this.calculatorForm.get('tipPercentage')?.value || 0;
-    const numOfPeople = this.calculatorForm.get('numOfPeople')?.value || 1;
+  getFieldError(fieldName: string) {
+    const field = this.calculatorForm.get(fieldName);
 
-    if ((e.target as HTMLElement).id === 'customPercentage') {
-      this.handleCustomPercentageChange(tipPercentage);
-    } else if ((e.target as HTMLInputElement).type === 'radio') {
-      this.handleRadioPercentageChange(tipPercentage);
+    if (!field?.touched && !field?.dirty) {
+      return '';
     }
 
-    let tipAmount = (billAmount * tipPercentage) / 100;
+    if (field?.hasError('required')) {
+      return "Can't be blank";
+    }
 
-    let total = billAmount + tipAmount;
+    if (field?.hasError('pattern')) {
+      if (field.errors!['pattern']['requiredPattern'] == '^[0-9]*$') {
+        return 'Must be integer';
+      }
+      return 'Invalid value';
+    }
 
+    if (field?.hasError('min')) {
+      const min = field.errors!['min'];
+      if (min['min'] == 1 && min['actual'] == 0) {
+        return "Can't be zero";
+      }
+
+      if (min['actual'] < 0) {
+        return "Can't be negative";
+      }
+
+      return 'Invalid value';
+    }
+    return '';
+  }
+
+  isFieldInvalid(fieldName: string) {
+    const field = this.calculatorForm.get(fieldName);
+    if (!field?.dirty && !field?.touched) {
+      return false;
+    }
+    const keys = Object.keys(field.errors || {});
+    return field?.invalid;
+  }
+
+  calculateOutput(): void {
+    if (this.calculatorForm.invalid) {
+      this.tipAmountPerPerson = 0;
+      this.totalPerPerson = 0;
+      return;
+    }
+    const billAmount = parseInt(
+      this.calculatorForm.get('billAmount')!.value || 0
+    );
+    const tipPercentage = Math.max(
+      parseInt(this.calculatorForm.get('tipPercentage')?.value || 0),
+      parseInt(this.calculatorForm.get('customTipPercentage')?.value || 0)
+    );
+
+    const numOfPeople = parseInt(this.calculatorForm.get('numOfPeople')!.value);
+
+    const tipAmount = billAmount * (tipPercentage / 100);
     this.tipAmountPerPerson = tipAmount / numOfPeople;
-    this.totalPerPerson = total / numOfPeople;
-
-    console.log(this.totalPerPerson);
+    this.totalPerPerson = (billAmount + tipAmount) / numOfPeople;
   }
 
-  handleCustomPercentageChange(tipPercentage: number) {
-    if (tipPercentage != this.PreviousTipPercentage && tipPercentage > 0) {
-      console.log(this.PreviousTipPercentage, tipPercentage);
-      const radios = this.elementRef.nativeElement.querySelectorAll(
-        'input[type="radio"]:checked'
-      );
-
-      console.log(radios);
-
-      radios?.forEach((radio: HTMLInputElement) => {
-        radio.checked = false;
-      });
+  uncheckTipRadios(): void {
+    if (this.calculatorForm.get('customTipPercentage')?.value !== '') {
+      this.calculatorForm.get('tipPercentage')?.setValue('');
     }
-    this.PreviousTipPercentage = tipPercentage;
   }
 
-  handleRadioPercentageChange(tipPercentage: number) {
-    const customPercentage = this.elementRef.nativeElement.querySelector(
-      '#customPercentage'
-    ) as HTMLInputElement;
+  emptyCustomTipPercentage(): void {
+    if (this.calculatorForm.get('tipPercentage')?.value !== '') {
+      this.calculatorForm.get('customTipPercentage')?.setValue('');
+    }
+  }
 
-    customPercentage.value = '';
-    this.PreviousTipPercentage = tipPercentage;
+  OnSubmit() {
+    this.resetForm();
   }
 }
